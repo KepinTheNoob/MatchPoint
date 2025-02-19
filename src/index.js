75746397db4142d636d7eb9e9dd1b226b7a44c55
Tesdv <76723396+Tesdv@users.noreply.github.com>
2025-02-19 23:22:47 +0700

    user val separation

--- src/index.js
+++ src/index.js
@@ -1,26 +1,10 @@
-import { PrismaClient } from '@prisma/client/edge'
-import { withAccelerate } from '@prisma/extension-accelerate'
+import express from 'express';
+import userRoutes from './users.js';
 
-const prisma = new PrismaClient().$extends(withAccelerate())
-
-const express = require('express');
-const { PrismaClient } = require('@prisma/client');
 const app = express();
 
 app.use(express.json());
-
-app.get('/users', async (req, res) => {
-  const users = await prisma.user.findMany();
-  res.json(users);
-});
-
-app.post('/users', async (req, res) => {
-  const { name, email } = req.body;
-  const user = await prisma.user.create({
-    data: { name, email },
-  });
-  res.json(user);
-});
+app.use('/users', userRoutes);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
