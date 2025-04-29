5433149d827d5b2f3beb3c9856813e19189800e0
JerJermyS <jeremy.jahja8@gmail.com>
2025-04-29 13:23:13 +0700

    [SCRUM-0] Added Home Screen Stateful Foundation

---
+++ lib/Home_Screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>{
+  bool test = 1;
+}
+
